073adf216aed3eb6dba62523e443ede9bdcf4435
Ringku 13 <ringku@softbdltd.com>
2020-01-02 12:51:45 +0600

    Second Activity  design completed

--- app/src/main/java/com/example/weathers/MainActivity.java
+++ app/src/main/java/com/example/weathers/MainActivity.java
@@ -3,17 +3,35 @@ package com.example.weathers;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+        TextView zone;
+        Context context=this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
+
+        zone=(TextView) findViewById(R.id.zone);
+
+        zone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(context,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
 
     }
 }
--- app/src/main/java/com/example/weathers/SecondActivity.java
+++ app/src/main/java/com/example/weathers/SecondActivity.java
@@ -3,12 +3,15 @@ package com.example.weathers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.WindowManager;
 
 public class SecondActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
     }
 }
