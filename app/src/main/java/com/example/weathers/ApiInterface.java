1dd2f82eb76d860d5da0a379e3514b74a149b348
Mohaimenul Reza <reza.softbdltd@gmail.com>
2020-01-05 16:33:49 +0600

    solved

--- app/src/main/java/com/example/weathers/ApiInterface.java
+++ app/src/main/java/com/example/weathers/ApiInterface.java
@@ -1,15 +1,14 @@
 package com.example.weathers;
 
 import retrofit2.Call;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface ApiInterface {
 
-    String BASE_URL="https://openweathermap.org/";
+    String BASE_URL = "https://openweathermap.org/";
 
 
-    @GET("data/2.5/weather?")
-    Call<WeatherPojo> getWeather(@Query("lat") String lat, @Query("lon") String lon,@Query("APPID") String app_id);
+    @GET("data/2.5/weather")
+    Call<WeatherPojo> getWeather(@Query("lat") String lat, @Query("lon") String lon, @Query("appid") String app_id);
 }
--- app/src/main/java/com/example/weathers/SecondActivity.java
+++ app/src/main/java/com/example/weathers/SecondActivity.java
@@ -1,12 +1,13 @@
 package com.example.weathers;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.os.Bundle;
-import android.text.Layout;
 import android.util.Log;
 import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,58 +18,61 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SecondActivity extends AppCompatActivity {
-    List<ImagePojo> images;
-     RecyclerView recycler;
-    Context context;
+
+    final String app_id = "b6907d289e10d714a6e88b30761fae22";
+    final String lat = "35";
+    final String lon = "139";
+    private List<ImagePojo> images;
+    private RecyclerView recycler;
+    private Context context;
+    private WeatherPojo weather;
     private ImageAdapter imgadapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-          final String app_id = "2e65127e909e178d0af311a81f39948c";
-          final String lat = "35";
-          final String lon = "139";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        images=new ArrayList<>();
+        images = new ArrayList<>();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-        recycler=(RecyclerView) findViewById(R.id.recycle);
-        imgadapter=new ImageAdapter(images,context);
+        recycler = findViewById(R.id.recycle);
+        imgadapter = new ImageAdapter(images, context);
         recycler.setAdapter(imgadapter);
         //Retrofit Object creating
-        Retrofit retrofit=new Retrofit.Builder().baseUrl(ApiInterface.BASE_URL)
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(ApiInterface.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-        ApiInterface api=retrofit.create(ApiInterface.class);
+        ApiInterface api = retrofit.create(ApiInterface.class);
         Call<WeatherPojo> call;
 
-        call=api.getWeather(lat,lon,app_id);
+        call = api.getWeather(lat, lon, app_id);
         call.enqueue(new Callback<WeatherPojo>() {
             @Override
             public void onResponse(Call<WeatherPojo> call, Response<WeatherPojo> response) {
-                WeatherPojo weather = response.body();
+                weather = response.body();
 
-                Log.d("Weather","Result "+weather.getMain().);
+                Log.d("Weather", "Result " + weather.getMain().getTemp());
             }
 
             @Override
             public void onFailure(Call<WeatherPojo> call, Throwable t) {
-                Log.d("Weather", "NO "+t);
+                Log.d("Weather", "NO " + t);
             }
         });
 
 
-        ImagePojo img=new ImagePojo(R.drawable.ic_beach_sunset);
+        ImagePojo img = new ImagePojo(R.drawable.ic_beach_sunset);
         images.add(img);
-        ImagePojo imgs=new ImagePojo(R.drawable.ic_ph_sunset);
+        ImagePojo imgs = new ImagePojo(R.drawable.ic_ph_sunset);
         images.add(imgs);
-        ImagePojo imgse=new ImagePojo(R.drawable.ic_sunrise);
+        ImagePojo imgse = new ImagePojo(R.drawable.ic_sunrise);
         images.add(imgse);
-        ImagePojo imgss=new ImagePojo(R.drawable.ic_sunset);
+        ImagePojo imgss = new ImagePojo(R.drawable.ic_sunset);
         images.add(imgss);
-        ImagePojo imgser=new ImagePojo(R.drawable.ic_sunrise);
+        ImagePojo imgser = new ImagePojo(R.drawable.ic_sunrise);
         images.add(imgser);
-        ImagePojo imgt=new ImagePojo(R.drawable.ic_sunrise_pink);
+        ImagePojo imgt = new ImagePojo(R.drawable.ic_sunrise_pink);
         images.add(imgt);
     }
 }
